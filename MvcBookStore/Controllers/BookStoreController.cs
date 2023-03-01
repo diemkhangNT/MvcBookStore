aa45eb5be564d40da7b630e98a5bedc82065cb9b
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-03-01 09:03:20 +0700

    Add project files.

---
+++ MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBookStore.Models;
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        QLBANSACHEntities db = new QLBANSACHEntities();
+
+        private List<SACH> LaySachMoi(int soluong)
+        {
+            //Sắp xếp sách theo ngày cập nhật giảm dần, lấy dúng số lượng sách cần 
+            //Chuyển qua dạng danh sách kết quả đạt được
+            return db.SACHes.OrderByDescending(sach => sach.NgayCapNhat).Take(soluong).ToList();
+        }
+
+
+        public ActionResult LayChuDe()
+        {
+            var dsChuDe = db.CHUDEs.ToList();
+            return PartialView(dsChuDe);
+        }
+
+        public ActionResult LayNXB()
+        {
+            var dsNXB = db.NHAXUATBANs.ToList();
+            return PartialView(dsNXB);
+        }
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            //Giả sử cần lấy 5 quyển sách mới cập nhật
+            var dsSachMoi = LaySachMoi(6);
+            return View(dsSachMoi);
+        }
+    }
+}
---
+++ MvcBookStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcBookStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MvcBookStore")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f413bb0e-82bf-4e0e-a835-e8a5f6db676c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
