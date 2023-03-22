674b207bb0ca3db429f8073f758311318a500b93
Nguyễn Thị Diễm Khang <diemkhang1912@gmail.com>
2023-03-22 08:41:29 +0700

    buoi7_ShoppingCart

---
+++ MvcBookStore/Controllers/GioHangController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBookStore.Models;
+
+namespace MvcBookStore.Controllers
+{
+    public class GioHangController : Controller
+    {
+         public List<MatHangMua> LayGioHang()
+        {
+            List<MatHangMua> giohang = Session["GioHang"] as List<MatHangMua>;
+            //Nếu giỏ hàng chưa tồn tại thì tạo mới và đưa vào Session
+            if (giohang == null)
+            {
+                giohang = new List<MatHangMua>();
+                Session["GioHang"] = giohang;
+            }
+            return giohang;
+        }
+
+        public ActionResult ThemSPVaoGio(int maSP)
+        {
+            //Lấy giỏ hàng hiện tại
+            List<MatHangMua> giohang = LayGioHang();
+
+            //Kiểm tra xem có tồn tại mặt hàng này trong giỏ hay chưa
+            //nếu có thì tăng số lượng lên 1, ngược lại thêm vào giỏ
+            MatHangMua sanPham = giohang.FirstOrDefault(s => s.MaSach == maSP);
+            if(sanPham == null)
+            {
+                sanPham = new MatHangMua(maSP);
+                giohang.Add(sanPham);
+            }
+            else
+            {
+                sanPham.SoLuong++; 
+            }
+            return RedirectToAction("Details", "BookStore", new { id = maSP });
+        }
+
+        private int TinhTongSL()
+        {
+            int tongSl = 0;
+            List<MatHangMua> giohang = LayGioHang();
+            if(giohang != null)
+            {
+                tongSl = giohang.Sum(sp => sp.SoLuong);
+            }
+            return tongSl;
+        }
+
+        private double TinhTongTien()
+        {
+            double TongTien = 0;
+            List<MatHangMua> giohang = LayGioHang();
+            if(giohang != null)
+            {
+                TongTien = giohang.Sum(sp => sp.ThanhTien());
+            }
+            return TongTien;
+        }
+
+        public ActionResult HienThiGioHang()
+        {
+            List<MatHangMua> giohang = LayGioHang();
+            //nếu giỏ hàng trống thì trả về trang ban đầu
+            if(giohang == null || giohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+            ViewBag.TongSL = TinhTongSL();
+            ViewBag.TongTien = TinhTongTien();
+            return View(giohang);
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSL = TinhTongSL();
+            ViewBag.TongTien = TinhTongTien();
+            return PartialView();
+        }
+    }
+}
---
+++ MvcBookStore/Models/MatHangMua.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBookStore.Models
+{
+    public class MatHangMua
+    {
+        QLBANSACHEntities db = new QLBANSACHEntities();
+        public int MaSach { get; set; }
+        public string TenSach { get; set; }
+        public string AnhBia { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public double ThanhTien()
+        {
+            return SoLuong * DonGia;
+        }
+
+        public MatHangMua(int MaSach)
+        {
+            this.MaSach = MaSach;
+            //Tìm sách trong csdl có mã id cần và gán cho mặt hàng được mua
+            var sach = db.SACHes.Single(s => s.MaSach == this.MaSach);
+            this.TenSach = sach.TenSach;
+            this.AnhBia = sach.HinhMinhHoa;
+            this.DonGia = double.Parse(sach.DonGia.ToString());
+            this.SoLuong = 1; 
+        }
+
+    }
+}
